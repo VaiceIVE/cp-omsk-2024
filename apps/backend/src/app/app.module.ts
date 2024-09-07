80d819cc33d66d4f43e5c5c4a7156b09239402cf
Ivan Isachenko <vanisachenko@gmail.com>
2024-09-08 01:41:39 +0600

    feat: nginx and some algo features

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { AuthModule } from '../auth/auth.module';
 import { StorageModule } from '../storage/storage.module';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import ormconfig from '../ormconfig';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { PresentationModule } from '../presentation/presentation.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig as TypeOrmModuleOptions),
     AuthModule, 
     StorageModule,
-    PresentationModule
+    PresentationModule,
   ],
   controllers: [],
   providers: [],
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,13 +8,17 @@ import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app/app.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(cookieParser())
-  app.use(bodyParser.json({limit: '100mb'}));
-  app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+  //app.use(bodyParser.json({limit: '100mb'}));
+  //app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+
   app.enableCors({
     credentials: true,
     origin:[
@@ -24,7 +28,9 @@ async function bootstrap() {
       "http://127.0.0.1:4200/",
     ],
   })
-
+  app.useStaticAssets(join(__dirname, '/static'), {
+    prefix: '/static/', index: false
+  });
   const port = process.env.PORT || 8000;
   await app.listen(port);
 }
